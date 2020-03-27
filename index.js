447f2b4011b6785b608dd713c08eeb8f0ab295dc
BuildTools <unconfigured@null.spigotmc.org>
2020-03-27 10:53:06 +0100

    Added a setting to remove unknown letters

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(config.token, {polling: true});
 const map = {"0":"⁰","1":"¹","2":"²","3":"³","4":"⁴","5":"⁵","6":"⁶","7":"⁷","8":"⁸","9":"⁹","a":"ᵃ","b":"ᵇ","c":"ᶜ","d":"ᵈ","e":"ᵉ","f":"ᶠ","g":"ᵍ","h":"ʰ","i":"ᶦ","j":"ʲ","k":"ᵏ","l":"ˡ","m":"ᵐ","n":"ⁿ","o":"ᵒ","p":"ᵖ","q":"ᑫ","r":"ʳ","s":"ˢ","t":"ᵗ","u":"ᵘ","v":"ᵛ","w":"ʷ","x":"ˣ","y":"ʸ","z":"ᶻ","A":"ᴬ","B":"ᴮ","C":"ᶜ","D":"ᴰ","E":"ᴱ","F":"ᶠ","G":"ᴳ","H":"ᴴ","I":"ᴵ","J":"ᴶ","K":"ᴷ","L":"ᴸ","M":"ᴹ","N":"ᴺ","O":"ᴼ","P":"ᴾ","Q":"Q","R":"ᴿ","S":"ˢ","T":"ᵀ","U":"ᵁ","V":"ⱽ","W":"ᵂ","X":"ˣ","Y":"ʸ","Z":"ᶻ","+":"⁺","-":"⁻","=":"⁼","(":"⁽",")":"⁾", "q":"ᵠ", "Q":"ᵠ", "?":"ˀ", "!":"ᵎ", " ":" "};
 
+const swapFunction = config.remove_unknown ? (c) => {return map[c]} : (c) => {let newLetter = map[c]; return newLetter === undefined ? c : map[c]};
+
 bot.on('message', (msg) => {
     bot.sendMessage(
-        msg.chat.id, (msg.text.split("").map((c) => {return map[c]})).join("")
+        msg.chat.id, (msg.text.split("").map(swapFunction)).join("")
     );
 });
